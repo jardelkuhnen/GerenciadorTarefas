311508e2757c3c6671c1a020aa1adb2b24453d39
Jardel Back Kuhnen <jardelkuhnen@gmail.com>
2019-05-08 21:18:39 -0300

    Adicionado funcionalidade ao status

--- src/app/tarefas/listar/listar-tarefa.component.ts
+++ src/app/tarefas/listar/listar-tarefa.component.ts
@@ -23,4 +23,19 @@ export class ListarTarefaComponent implements OnInit {
     return this.tarefaService.listarTodos();
   }
 
+  remover($event: any, tarefa: Tarefa): void {
+    $event.preventDefault();//Bloqueia a atualizaçao da pagina automatica quando clicado no botao remover 
+    if(confirm('Deseja remover a tarefa: "' + tarefa.nome + '" ?')){
+      this.tarefaService.remover(tarefa.id);
+      this.tarefas = this.tarefaService.listarTodos();
+    }
+  }
+
+  alterarStatus(tarefa: Tarefa): void {
+    if(confirm('Deseja alterar o status da tarefa "' + tarefa.nome + '" ?')){
+      this.tarefaService.atualizaStatus(tarefa.id);
+      this.tarefas = this.tarefaService.listarTodos();      
+    }
+  }    
+  
 }
