7ea1b9bdd902a1cf87f961127c39052e1fd66e57
Jardel Back Kuhnen <jardelkuhnen@gmail.com>
2019-05-16 22:37:17 -0300

    Adicionado loadById

--- src/app/tarefas/editar/editar-tarefa.component.ts
+++ src/app/tarefas/editar/editar-tarefa.component.ts
@@ -29,7 +29,7 @@ export class EditarTarefaComponent implements OnInit {
   atualizar(): void {
     if(this.formTarefa.form.valid){
       this.tarefaService.atualizar(this.tarefa);
-      this.router.navigate(['/tarefas']);
+      // this.router.navigate(['/tarefas']);
     }
   }
 
--- src/app/tarefas/shared/tarefa.service.ts
+++ src/app/tarefas/shared/tarefa.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 
 import { Tarefa } from './tarefa.model';
 import { Observable } from 'rxjs';
@@ -8,13 +8,15 @@ import { HttpParams } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
-export class TarefaService {
-
+export class TarefaService { 
+  
+  
+  
   constructor(private httpClient: HttpClient) { }
-
+  
   baseUrl = 'http://localhost:8080/api/tasks';
-
-  listarTodos(): Observable<any> {
+  
+  listarTodos(): Observable<Tarefa[]> {
       // const tarefas = localStorage['tarefas'];
       // return tarefas ? JSON.parse(tarefas) : [];
       let url = this.baseUrl + '/listAll';
@@ -33,7 +35,18 @@ export class TarefaService {
   buscarPorId(id: number): Tarefa {
     // const tarefas: Tarefa[] = this.listarTodos();
     // return tarefas.find(tarefa => tarefa.id === id);
-    return null;
+    // let url = this.baseUrl + '/loadById';
+    // const params = new HttpParams().set('id', JSON.stringify(id));
+    // return this.httpClient.get<Tarefa[]>(url, {params: params});
+    // this.listarTodos().subscribe(tarefas => this.tarefas = tarefas);
+    let url = this.baseUrl + '/loadById';
+    const tarefa: Tarefa = null;
+
+    const params = new HttpParams().set('id', JSON.stringify(id));
+    this.httpClient.get<Tarefa>(url, {params}).subscribe(tarefa => tarefa = tarefa);
+    console.log('Tarefas<br/> ' + tarefa.nome);
+    return tarefa;
+
   }
 
   atualizar(tarefa: Tarefa): void{
@@ -44,6 +57,9 @@ export class TarefaService {
     //     }
     // });
     // localStorage['tarefas'] = JSON.stringify(tarefas);
+    let url = this.baseUrl + '/update';
+    this.httpClient.post<Tarefa>(url, tarefa).subscribe(r=>{});
+
   }
 
   remover(id: number): void{
